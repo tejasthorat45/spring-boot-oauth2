181e20a4a7a04f49febfeaad7a107373e29886e9
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2025-02-03 17:10:38 +0530

    Add files via upload

---
+++ spring-boot-Oauth/spring-boot-Oauth/src/main/java/com/springboot/oauth/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.springboot.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @GetMapping("/")
+    public String getmsg(){
+
+        return "Hello from DemoController!";
+    }
+}
